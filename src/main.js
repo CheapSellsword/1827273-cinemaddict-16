b9039c5e497a0ed2b3585f3fb61a91280898b6c3
Al Much <almuchwork@gmail.com>
2022-01-25 20:55:44 +0300

    Удалит моки

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import { GENERATED_FILMS_COUNT, AUTHORIZATION, END_POINT } from './consts';
+import { AUTHORIZATION, END_POINT } from './consts';
 import { RenderPosition, render } from './utils/render';
-import { generateFilm } from './mock/film';
 import FilmCountView from './view/film-count-view';
 import ProfileRankAndAvatarView from './view/profile-and-rank-avatar-view';
 import FilmCollectionPresenter from './presenter/film-collection-presenter';
@@ -10,18 +9,15 @@ import FilterPresenter from './presenter/filter-presenter';
 import CommentsModel from './model/comments-model';
 import ApiService from './api-service.js';
 
-const films = Array.from({length: GENERATED_FILMS_COUNT}, generateFilm);
-
-const apiServiceComponent = new ApiService(END_POINT, AUTHORIZATION);
-const commentsModel = new CommentsModel(apiServiceComponent);
-const filmsModel = new FilmsModel(apiServiceComponent, commentsModel);
-
-const filterModel = new FilterModel();
-
 const headerElement = document.querySelector('.header');
 export const mainElement = document.querySelector('.main');
 const footerElement = document.querySelector('.footer');
 const footerStatsElement = footerElement.querySelector('.footer__statistics');
+
+const apiServiceComponent = new ApiService(END_POINT, AUTHORIZATION);
+const commentsModel = new CommentsModel(apiServiceComponent);
+const filmsModel = new FilmsModel(apiServiceComponent, commentsModel);
+const filterModel = new FilterModel();
 
 const filmCollectionPresenter = new FilmCollectionPresenter(mainElement, filmsModel, filterModel, commentsModel);
 const filterPresenter = new FilterPresenter(mainElement, filterModel, filmsModel);
--- src/utils/dates-and-time.js
+++ src/utils/dates-and-time.js
@@ -1,5 +1,4 @@
-import { MIN_FILM_LENGTH, MAX_FILM_LENGTH, MIN_NUMBER, DATE_GAP, HOUR_IN_MINS} from '../consts';
-import { getRandomInteger } from './common';
+import { HOUR_IN_MINS } from '../consts';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import durationPlugin from 'dayjs/plugin/duration';
@@ -13,37 +12,13 @@ export const convertMinutes = (num) => {
   return `${hours  }h ${  minutes}m`;
 };
 
-export const getFilmLengthInMinutes = () => getRandomInteger(MIN_FILM_LENGTH, MAX_FILM_LENGTH);
-
-export const generateFilmLength = () => {
-  const filmLengthInMinutes = getRandomInteger(MIN_FILM_LENGTH, MAX_FILM_LENGTH);
-
-  const convertMinutes = (num) => {
-    const hours = Math.floor(num / HOUR_IN_MINS);
-    const minutes = num % HOUR_IN_MINS;
-    return `${hours  }h ${  minutes}m`;
-  };
-
-  return convertMinutes(filmLengthInMinutes);
-};
-
-export const generateFullReleaseDate = () => {
-  const randomDate = dayjs()
-    .subtract(getRandomInteger(MIN_NUMBER, DATE_GAP), 'year')
-    .subtract(getRandomInteger(MIN_NUMBER, DATE_GAP), 'month')
-    .subtract(getRandomInteger(MIN_NUMBER, DATE_GAP), 'day');
-  return randomDate.format('DD MMMM YYYY');
-};
-
-export const generateYearOnly = () => {
-  const randomDate = dayjs()
-    .subtract(getRandomInteger(MIN_NUMBER, DATE_GAP), 'year');
-  return randomDate.format('YYYY');
-};
-
 export const getHumanizedTimeOfComments = (date) => {
   const currentTime = dayjs();
   const minutesDifference = currentTime.diff(date, 'minute');
-
   return dayjs.duration(-minutesDifference, 'minutes').humanize(true);
+};
+
+export const getFilmLengthInMinutes = (filmLength) => {
+  const filmLengthNumbers = filmLength.replace(/\D/g, '').toString();
+  return Number(filmLengthNumbers.slice(1)) + Number(filmLengthNumbers.charAt(0) * 60);
 };
